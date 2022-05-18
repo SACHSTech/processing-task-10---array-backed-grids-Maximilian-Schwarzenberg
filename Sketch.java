1e1bae1de34f2db69a52c6d8a6171a60a41e28b8
Maximilian Schwarzenberg <78431850+Maximilian-Schwarzenberg@users.noreply.github.com>
2022-05-18 17:52:25 +0000

    Level 3 Completed

--- Sketch.java
+++ Sketch.java
@@ -12,7 +12,7 @@ public class Sketch extends PApplet {
   int SCREEN_WIDTH = (CELL_WIDTH + MARGIN) * ROW_COUNT + MARGIN; // Calculates the width of the screen
   int SCREEN_HEIGHT = (CELL_HEIGHT + MARGIN) * COLUMN_COUNT + MARGIN; // Calculates the height of the screen
 
-  int[][] intGrid = new int [ROW_COUNT][COLUMN_COUNT];
+  int[][] intGrid = new int [ROW_COUNT][COLUMN_COUNT]; // Array responsible for the coloring of the grid
 
   
   /**
@@ -21,13 +21,6 @@ public class Sketch extends PApplet {
   public void settings() {
     // put your size call here
     size(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    /*
-    for (int CellX = MARGIN + (CELL_WIDTH/2); intRow < (ROW_COUNT * (CELL_WIDTH + MARGIN)); CellX += MARGIN + (CELL_WIDTH/2)) {
-      System.out.println("CellX: " + CellX);
-    }
-    */
-    
   }
 
   /** 
@@ -36,44 +29,28 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(0);
-
-    intGrid[1][5] = 1;
-
     
-    for (int intGridRow = MARGIN + (CELL_WIDTH/2); intGridRow < (ROW_COUNT * (CELL_WIDTH + MARGIN)); intGridRow += MARGIN + (CELL_WIDTH/2)) {
-      for (int intGridColumn = MARGIN + (CELL_HEIGHT/2); intGridColumn < (COLUMN_COUNT * (CELL_HEIGHT + MARGIN)); intGridColumn += MARGIN + (CELL_HEIGHT/2)) {
-        /*
-        //System.out.println("intGridRow: " + intGridRow);
-        //System.out.println("intGridColumn: " + intGridColumn);
-
-        
-        if (mouseX >= intArGridRow[intGridRow]-10 && mouseX <= intArGridRow[intGridRow]+10) {
-          System.out.println("working: " + intArGridRow[intGridRow]);
-        }
-        */
-        
-        
-      }
-    }
-    
-    
+    //intGrid[1][5] = 1;   
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  int intX = 0;
-    int intY = 0;
 
-    for (int intRow = 0; intRow < (ROW_COUNT * (CELL_WIDTH + MARGIN)); intRow+=CELL_WIDTH + MARGIN) {
-      for (int intColumn = 0; intColumn < (COLUMN_COUNT * (CELL_HEIGHT + MARGIN)); intColumn+=CELL_HEIGHT + MARGIN) {
-        intX = MARGIN + intRow;
-        intY = MARGIN + intColumn;
-  
-        fill(255);
-        noStroke();
-        rect(intX, intY, CELL_WIDTH, CELL_HEIGHT);
+    for (int intRow = 0; intRow < ROW_COUNT; intRow++) {
+      for (int intColumn = 0; intColumn < COLUMN_COUNT; intColumn++) {
+        if (intGrid[intRow][intColumn] == 1) {
+          fill(0, 255, 0);
+          noStroke();
+          rect(intRow * (CELL_WIDTH + MARGIN) + MARGIN, intColumn * (CELL_HEIGHT + MARGIN) + MARGIN, CELL_WIDTH, CELL_HEIGHT);
+        }
+        else {
+          fill(255);
+          noStroke();
+          rect(intRow * (CELL_WIDTH + MARGIN) + MARGIN, intColumn * (CELL_HEIGHT + MARGIN) + MARGIN, CELL_WIDTH, CELL_HEIGHT);
+        }
+        
       }
     }
     
@@ -81,7 +58,23 @@ public class Sketch extends PApplet {
 
   public void mousePressed(){
 
-    System.out.println("Click coords: (" + mouseX + ", " + mouseY + ") ; Grid coords: (row:" + mouseX + ", column:" + mouseY + ")");
+    for (int intRow = 0; intRow < ROW_COUNT; intRow++) {
+      for (int intColumn = 0; intColumn < COLUMN_COUNT; intColumn++) {
+        if ((mouseX > intRow * (CELL_WIDTH + MARGIN) + MARGIN && mouseX < intRow * (CELL_WIDTH + MARGIN) + MARGIN + CELL_WIDTH) && (mouseY > intColumn * (CELL_HEIGHT + MARGIN) + MARGIN && mouseY < intColumn * (CELL_HEIGHT + MARGIN) + MARGIN + CELL_HEIGHT)) {
+          System.out.println("Click coords: (" + mouseX + ", " + mouseY + ") ; Grid coords: (row:" + (intRow+1) + ", column:" + (intColumn+1) + ")");
+
+          if (intGrid[intRow][intColumn] == 0) {
+            intGrid[intRow][intColumn] = 1;
+          }
+          else if (intGrid[intRow][intColumn] == 1) {
+            intGrid[intRow][intColumn] = 0;
+          }
+          
+        }
+        
+      }
+    }
+
     
   }
   
