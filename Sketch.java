48862f7d34aba3d80dfb76e22cb0a2ace595dbce
Maximilian Schwarzenberg <78431850+Maximilian-Schwarzenberg@users.noreply.github.com>
2022-05-20 17:14:09 +0000

    Done level 4

--- Sketch.java
+++ Sketch.java
@@ -15,6 +15,9 @@ public class Sketch extends PApplet {
   int[][] intGrid = new int [ROW_COUNT][COLUMN_COUNT]; // Array responsible for the coloring of the grid
 
   int cellSelected = 0; // The amount of cells selected
+  int rowSelected = 0; // The amount of cells selected in a row
+  int columnSelected = 0; // The amount of cells selected in a column
+  int continueSelected = 0; // The amount of cells selected continuously in a row
 
   
   /**
@@ -52,7 +55,7 @@ public class Sketch extends PApplet {
           noStroke();
           rect(intRow * (CELL_WIDTH + MARGIN) + MARGIN, intColumn * (CELL_HEIGHT + MARGIN) + MARGIN, CELL_WIDTH, CELL_HEIGHT);
         }
-        
+
       }
     }
     
@@ -63,8 +66,9 @@ public class Sketch extends PApplet {
     for (int intRow = 0; intRow < ROW_COUNT; intRow++) {
       for (int intColumn = 0; intColumn < COLUMN_COUNT; intColumn++) {
         if ((mouseX > intRow * (CELL_WIDTH + MARGIN) + MARGIN && mouseX < intRow * (CELL_WIDTH + MARGIN) + MARGIN + CELL_WIDTH) && (mouseY > intColumn * (CELL_HEIGHT + MARGIN) + MARGIN && mouseY < intColumn * (CELL_HEIGHT + MARGIN) + MARGIN + CELL_HEIGHT)) {
-          System.out.println("Click coords: (" + mouseX + ", " + mouseY + ") ; Grid coords: (row:" + (intRow+1) + ", column:" + (intColumn+1) + ")");
+          System.out.println("Click coords: (" + mouseX + ", " + mouseY + ") ; Grid coords: (row:" + (intColumn+1) + ", column:" + (intRow+1) + ")");
 
+           // Main block
           if (intGrid[intRow][intColumn] == 0) {
             intGrid[intRow][intColumn] = 1; // if the cell isnt selected itll be selected
             cellSelected++; // adds one to the cell counter
@@ -73,51 +77,111 @@ public class Sketch extends PApplet {
             intGrid[intRow][intColumn] = 0; // if the cell is selected itll be unselected
             cellSelected--; // subtracts one to the cell counter
           }
-
-          if (intGrid[intRow+1][intColumn] == 0) {
+          
+           // Right block
+          if (intGrid[intRow+1][intColumn] == 0) { // && intRow < 8
             intGrid[intRow+1][intColumn] = 1;
             cellSelected++; // adds one to the cell counter
           }
-          else if (intGrid[intRow+1][intColumn] == 1) {
+          else if (intGrid[intRow+1][intColumn] == 1) { // && intRow < 8
             intGrid[intRow+1][intColumn] = 0;
             cellSelected--; // subtracts one to the cell counter
           }
           
-          if (intGrid[intRow-1][intColumn] == 0) {
+           // Left block
+          if (intGrid[intRow-1][intColumn] == 0) { // && intRow > 1
             intGrid[intRow-1][intColumn] = 1;
             cellSelected++; // adds one to the cell counter
           }
-          else if (intGrid[intRow-1][intColumn] == 1) {
+          else if (intGrid[intRow-1][intColumn] == 1) { // && intRow > 1
             intGrid[intRow-1][intColumn] = 0;
             cellSelected--; // subtracts one to the cell counter
           }
-          
-          if (intGrid[intRow][intColumn+1] == 0) {
+
+           // Bottom block
+          if (intGrid[intRow][intColumn+1] == 0) { // && intColumn < 8
             intGrid[intRow][intColumn+1] = 1;
             cellSelected++; // adds one to the cell counter
           }
-          else if (intGrid[intRow][intColumn+1] == 1) {
+          else if (intGrid[intRow][intColumn+1] == 1) { // && intColumn < 8
             intGrid[intRow][intColumn+1] = 0;
             cellSelected--; // subtracts one to the cell counter
           }
 
-          if (intGrid[intRow][intColumn-1] == 0) {
+           // Top block
+          if (intGrid[intRow][intColumn-1] == 0) { // && intColumn > 1
             intGrid[intRow][intColumn-1] = 1;
             cellSelected++; // adds one to the cell counter
           }
-          else if (intGrid[intRow][intColumn-1] == 1) {
+          else if (intGrid[intRow][intColumn-1] == 1) { // && intColumn > 1
             intGrid[intRow][intColumn-1] = 0;
             cellSelected--; // subtracts one to the cell counter
           }
+          
 
+           // Output of the cells selected
           System.out.println("Total of " + cellSelected + " cells are selected.");
           System.out.println("");
-          
         }
         
       }
     }
 
+     // The amount of cells selected in a row
+    for (int intRow = 0; intRow < ROW_COUNT; intRow++) {
+      for (int intColumn = 0; intColumn < COLUMN_COUNT; intColumn++) {
+        
+        if (intGrid[intRow][intColumn] == 1) {
+          rowSelected++;
+        }
+
+        if (intColumn < COLUMN_COUNT-1) {
+          if (intGrid[intRow][intColumn] == 1 && intGrid[intRow][intColumn+1] == 1) {
+            continueSelected++;
+          }
+          
+        }
+
+        if (intColumn > 0 && intColumn < COLUMN_COUNT) {
+          if (intGrid[intRow][intColumn-1] == 1 && intGrid[intRow][intColumn] == 1 && intColumn == COLUMN_COUNT-1) {
+            continueSelected++;
+          }
+          else if (intGrid[intRow][intColumn-1] == 1 && intGrid[intRow][intColumn] == 1 && intGrid[intRow][intColumn+1] == 1 && intColumn < COLUMN_COUNT-1) {
+            continueSelected++;
+          }
+          
+        }
+        
+      }
+
+      if (continueSelected > 0 && rowSelected > 2) {
+        System.out.println("There are " + continueSelected + " continuous cells are selected in column " + (intRow+1) + ".");
+      }
+      
+      System.out.println("Total of " + rowSelected + " cells are selected in column " + (intRow+1) + ".");
+    
+      continueSelected = 0;
+      rowSelected = 0;
+    }
+    
+    System.out.println("");
+
+     // The amount of cells selected in a column
+    for (int intColumn = 0; intColumn < COLUMN_COUNT; intColumn++) {
+      for (int intRow = 0; intRow < ROW_COUNT; intRow++) {
+        
+        if (intGrid[intRow][intColumn] == 1) {
+          columnSelected++;
+        }
+        
+      }
+      
+      System.out.println("Total of " + columnSelected + " cells are selected in row " + (intColumn+1) + ".");
+    
+      columnSelected = 0;
+    }
+
+    System.out.println("");
     
   }
   
